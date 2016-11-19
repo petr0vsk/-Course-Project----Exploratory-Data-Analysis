2e188d9e76ed0ff43b175fb2b0cfd97b5956f443
Aleksander <petr0vskjy.aleksander@gmail.com>
2016-11-19 14:23:53 +0400

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -1,1 +1,25 @@
 
+# --------------------------------------------
+# Exploratory Data Analysis, Week 1,  Course Project
+# petr0vskjy.aleksander@gmail.com
+# Plot 1
+# -------------------------------------------
+#clear memory
+rm(list=ls())
+# file with function`s
+source("/home/petr0vsk/Project1/global.R")
+#
+library(tidyr)
+library(dplyr)
+library(plyr)
+library(stringr)
+library(sqldf)
+
+# set directory
+set.right.directory()
+# get list of clear data
+power.list <- read.and.save.data()
+# make plot
+png("plot1.png", width = 480, height = 480)
+hist(power.list$active.power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
